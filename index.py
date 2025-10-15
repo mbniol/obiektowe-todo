e9214b95e77a6e975021149209669c10b50fb142
Mateusz Biniak <matbin31@gmail.com>
2025-10-15 10:05:53 +0200

    Refactor app execution to use threading for improved responsiveness and open browser automatically on start

--- index.py
+++ index.py
@@ -2,11 +2,30 @@ import modules.objectLib as ol
 from classes.Priority import Priority
 from classes.EditType import EditType
 from web.webHost import app
-import asyncio
+import threading
+import webbrowser
+import time
 
 
-async def runApp():
+def runApp():
     app.run()
     
+def runBrowser():
+    url = "http://127.0.0.1:5000"
+    webbrowser.open(url,new=0, autoraise=True)
+
+
 if __name__ == "__main__":
-    asyncio.run(runApp())
+    thread_run_app = threading.Thread(target=runApp)
+    thread_run_app.daemon = True
+
+
+    thread_run_app.start()
+    print("App thread started")
+
+    runBrowser()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Zamykanie programu...")
