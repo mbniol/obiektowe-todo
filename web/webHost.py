da39b558e09d7b3cefae30a2a8c0b1b45d59e7c0
Mateusz Biniak <matbin31@gmail.com>
2025-10-25 08:44:00 +0200

    Add 404 error page with styles and assets

--- web/webHost.py
+++ web/webHost.py
@@ -5,5 +5,10 @@ app = flask.Flask(__name__)
 db=ol.dbSelfHost()
 
 @app.route("/")
-def hello_world():
+def index():
     return flask.render_template('index.html', getall=ol.getAll(db))
+
+
+@app.errorhandler(404)
+def not_found(e):
+  return flask.render_template("404.html")
